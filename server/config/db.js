ade8f2e3f52aa3528216ed3675411cadcf53b7c8
BARBARIK12345 <ktotre@gmail.com>
2024-04-27 19:03:56 +0530

    Connect to database

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+
+const connectDB = async()=>{
+   try{
+    await mongoose.connect("mongodb://localhost:27017")
+    console.log("Database is coneected")  
+
+} catch(error){
+  console.log(`the Error is ${error}`)
+}
+
+};
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express= require("express")
 const cors = require("cors")
-
+const connectDB = require("./config/db")
 const app = express()
 
 app.use(cors())
+
+//==== Connect database ===/
+connectDB()
 
 app.get("/" , (req , res)=>{
     res.send("hii welcome to server")
